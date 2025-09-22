3b108b7944b32db4e1ff6e2900f00e92397748a0
Anna Ly <annahuynhly012920@gmail.comm>
2025-09-22 12:42:06 -0400

    minor revisions

--- coding/lecture/unit5.r
+++ coding/lecture/unit5.r
@@ -2,6 +2,12 @@
 # Unit 5 Notes               #
 ##############################
 
+# Suppose $X_{1} \sim N(-2, 1)$, $X_{2} \sim N(2, 1)$, and $X_{1} \ind X_{2}$. 
+# Simulating the following using $R$:
+#    F_{X}(x) := 0.5 F_{X_{1}}(x_{1}) + 0.5 F_{X_{2}}(x_{2})
+# Create three histograms; the first one shall have 10 bins, the second 25, 
+# and the third 50. What patterns do you notice?
+
 n <- 10000
 x1 <- rnorm(n, -2, 1)
 x2 <- rnorm(n, 2, 1)
@@ -29,4 +35,76 @@ curve(true_curve(x), col = "red", lwd = 2, add = TRUE)
 hist(x, prob = TRUE, ylim = c(0, 0.2), breaks = 50, 
      col = "chartreuse", border = "white")
 curve(true_curve(x), col = "red", lwd = 2, add = TRUE)
+
+# Generate a random sample from the standard Gaussian distribution. 
+# Compute the number of breaks in R manually using Struges' rule, and then 
+# compare this to the default settings of hist(). 
+
+par(mfrow = c(1, 3))
+
+n <- 10^5
+x <- rnorm(n)
+# Doing this manually
+nclass <- ceiling(1 + log2(n))
+breaks <- seq(min(x), max(x), diff(range(x) / nclass))
+# Using built-in R function
+h <- hist(x, prob = TRUE, col = "skyblue", border = "white")
+curve(dnorm(x), -4, 4, col = "red", lwd = 2, add = TRUE)
+
+# comparing the breaks 
+breaks 
+h$breaks
+# the reason why this is the case is because R actually uses a modified version
+# of Sturges' rule for it to look "better". Let's look at the source code:
+graphics::hist.default 
+
+# Generate a random sample from the standard Gaussian distribution. 
+# Compute the number of breaks manually in R using Scott's normal reference rule, 
+# and then compare this to setting hist($\cdot$, breaks = ``Scott").
+
+# Manually
+breaks2 <- seq(min(x), max(x), by = 3.49 * sd(x) * n^(-1/3))
+
+# Using R
+h2 <- hist(x, prob = TRUE, breaks = "Scott", 
+           col = "plum1", border = "white")
+curve(dnorm(x), col = "red", lwd = 2, add = TRUE)
+
+h2$breaks
+
+# They look a lot different; again, a lot of this comes from R trying to give 
+# more beautiful breaks...
+
+# Generate a random sample from the standard Gaussian distribution. 
+# Compute the number of breaks manually in R using the Freedman-Diaconis rule, 
+# and then compare this to setting hist($\cdot$, breaks = ``FD").
+
+# Manually
+breaks3 <- seq(min(x), max(x), by = 2 * IQR(x) * n^(-1/3))
+breaks3
+
+# Using R
+h3 <- hist(x, prob = TRUE, breaks = "FD", 
+           col = "chartreuse", border = "white")
+curve(dnorm(x), col = "red", lwd = 2, add = TRUE)
+h3$breaks
+
+# BELOW IS OPTIONAL...
+# Version 4 (according to the OG source, 2.59 should be used for FD,
+# but 2 was recommended by the authors.)
+
+par(mfrow = c(1, 1))
+
+# Manually
+breaks4 <- seq(min(x), max(x), by = 2.59 * IQR(x) * n^(-1/3))
+breaks4
+
+# Using R
+h4 <- hist(x, prob = TRUE, breaks = "FD", 
+           col = "green", border = "white")
+curve(dnorm(x), col = "red", lwd = 2, add = TRUE)
+h4$breaks
+
+
+
 
---
+++ unit6.r
@@ -0,0 +1,97 @@
+##############################
+# Unit 6 Notes               #
+##############################
+
+# Use optimize() to maximize this function with respect to $x$:
+# f(x) = \frac{\log(1 + \log(x))}{\log(1+x)}
+
+f <- function(x){
+ return(log(x + log(x))/log(1+x)) 
+}
+
+curve(f(x), from = 2, to = 15, ylab = "f(x)", lwd = 2,
+      ylim = c(0.9, 1.10))
+
+op <- optimize(f, lower = 2.4, upper = 8, maximum = TRUE)
+
+# obtain maximum
+op$maximum
+f(op$maximum)
+
+abline(h = f(op$maximum), col = "red", lty = 2, lwd = 2)
+
+# For $n = 10$ randomly sample from a $Gamma(\alpha = 5, \lambda = 2)$ using 
+# rgamma(). Use optim() to maximize this function with respect to 
+# $\boldsymbol{\theta} = (\alpha, \lambda)$
+# Compare the results and time required between the 5 different methods.
+
+samp <- rgamma(10000, shape = 5, rate = 2)
+
+gamma_likelihood = function(theta, sample){
+  alpha <- theta[1]
+  lambda <- theta[2]
+  n <- length(sample)
+  sum_xi <- sum(sample)
+  sum_log_xi <- sum(log(sample))
+  part1 <- alpha*n*log(lambda) - n * lgamma(alpha)
+  part2 <- (alpha-1)*sum_log_xi - lambda * sum_xi
+  # By default, optim() gives the MINIMUM. need to negate for the max!
+  return(-(part1 + part2))
+}
+
+start_val <- c(1, 1)
+
+op_1 <- optim(start_val, sample = samp, gamma_likelihood, method = "Nelder-Mead")
+op_2 <- optim(start_val, sample = samp, gamma_likelihood, method = "BFGS")
+op_3 <- optim(start_val, sample = samp, gamma_likelihood, method = "CG")
+op_4 <- optim(start_val, sample = samp, gamma_likelihood, method = "L-BFGS-B",
+              lower = c(1e-6, 1e-6))
+op_5 <- optim(start_val, sample = samp, gam_like_neg, method = "SANN")
+
+op_1$par
+op_2$par
+op_3$par
+op_4$par
+op_5$par
+
+# Use optim() to maximize this function with respect to $x$:
+#    f(x, n, r, t) = \left( 1 + \frac{n-r}{2} \right)x - r + 
+#    \left( \frac{n-r}{2} \right) t
+# Compare the results and time required between the 5 different methods.
+
+f2 <- function(theta){
+  x <- theta[1]
+  n <- theta[2]
+  r <- theta[3]
+  t <- theta[4]
+  part1 <- (1 + (n-r)/2) * x
+  part2 <- ((n-r)/2) * t
+  return(part1 - r + part2)
+}
+
+# By default, optim() gives the MINIMUM. need to negate for the max!
+f2_neg <-function(theta){return(-f2(theta))}
+
+# To use optim(), you need a reasonable starting point.
+# You can ignore how I'm picking these values; the function was
+# motivated from my thesis.
+n <- 20
+t <- 3
+r <- 5
+x <- sum(rexp(r))
+start_val <- c(sum(exp(r)), n, r, t)
+
+op_1 <- optim(start_val, f2_neg, method = "Nelder-Mead")
+op_2 <- optim(start_val, f2_neg, method = "BFGS")
+op_3 <- optim(start_val, f2_neg, method = "CG")
+op_4 <- optim(start_val, f2_neg, method = "L-BFGS-B")
+op_5 <- optim(start_val, f2_neg, method = "SANN")
+
+f2(op_1$par)
+f2(op_2$par)
+f2(op_3$par)
+f2(op_4$par)
+f2(op_5$par)
+
+
+
